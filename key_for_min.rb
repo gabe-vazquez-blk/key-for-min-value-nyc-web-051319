38d81c65af47f6aca96cf2d46c4192cc5f46616a
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-05 21:27:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   new_value = 501
-  name_hash.collect do |key, value|
+  name_hash.each do |key, value|
     if value < new_value
       new_value = value
       return key
