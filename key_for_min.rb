e8c29cbeff03fdd81105b6dacfc6c870694f7091
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-05 21:33:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_key = nil
   name_hash.each do |key, value|
-    if value < new_value
-      new_value = value
+    if value < lowest_value
+      lowest_value = value
       lowest_key = key
     end
   end
