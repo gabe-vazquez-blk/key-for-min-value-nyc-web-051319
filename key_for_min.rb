d7d761baae979ce3191592535c404b6108ed0f8b
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-05 21:16:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  new_value = 0
-  new_key = ""
-  name_hash.each do |key, value|
-    if value < new_value
-      new_value = value
-      new_key = key
-    end
-  end
+  name_hash
 end
